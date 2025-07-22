0d4736e1796f4fc48f704e6961c99ff85cfa8377
Tnaike <timmynaike@gmail.com>
2025-07-22 22:27:42 +0100

    Initial commit

---
+++ src/test/components/button.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { test, expect, vi } from "vitest";
+import React from "react";
+import { Button } from "../../components/ui/button";
+
+test("should renders button and handles click", async () => {
+  const user = userEvent.setup();
+  const onClick = vi.fn();
+
+  render(<Button onClick={onClick}>Click me</Button>);
+
+  expect(screen.getByText("Click me")).toBeTruthy();
+
+  await user.click(screen.getByText("Click me"));
+  expect(onClick).toHaveBeenCalledTimes(1);
+});
